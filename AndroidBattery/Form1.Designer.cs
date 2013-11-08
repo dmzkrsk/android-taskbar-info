da2fd84f851d8ba396a283c56c9a9953bcf80be3
dmzkrsk <devnull@localhost>
2013-11-08 12:17:20 +0800

    Initial

---
+++ AndroidBattery/Form1.Designer.cs
@@ -0,0 +1,71 @@
+﻿using System.Windows.Forms;
+
+namespace AndroidBattery
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.formProgressBar = new ProgressBar();
+            this.SuspendLayout();
+            // 
+            // formProgressBar
+            // 
+            this.formProgressBar.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.formProgressBar.Location = new System.Drawing.Point(0, 0);
+            this.formProgressBar.Name = "formProgressBar";
+            this.formProgressBar.Size = new System.Drawing.Size(384, 61);
+            this.formProgressBar.Style = System.Windows.Forms.ProgressBarStyle.Continuous;
+            this.formProgressBar.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(384, 61);
+            this.Controls.Add(this.formProgressBar);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(400, 100);
+            this.MinimumSize = new System.Drawing.Size(400, 100);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Android Battery Level";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Shown += new System.EventHandler(this.Form1_Shown);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private ProgressBar formProgressBar;
+
+    }
+}
+
---
+++ AndroidBattery/Form1.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.WindowsAPICodePack.Taskbar;
+
+namespace AndroidBattery
+{
+    public partial class Form1 : Form
+    {
+        private readonly Timer timer;
+        private int level = 0;
+        private readonly ProcessStartInfo startInfo;
+        private readonly TaskbarManager shellProgressBar;
+        private bool TaskBarAvailable = false;
+
+#if DEBUG
+        private const int TIMER_INTERVAL = 5000;
+#else
+        private const int TIMER_INTERVAL = 1000 * 60 * 2;
+#endif
+
+        public Form1()
+        {
+            startInfo = new ProcessStartInfo("adb", "shell cat /sys/class/power_supply/battery/capacity")
+            {
+                UseShellExecute = false,
+                RedirectStandardOutput = true,
+                RedirectStandardError = true,
+                CreateNoWindow = true,
+            };
+
+            InitializeComponent();
+
+            shellProgressBar = TaskbarManager.Instance;
+            shellProgressBar.ApplicationId = Application.ProductName;
+
+            timer = new Timer();
+        }
+
+        void timer_Tick(object sender, EventArgs e)
+        {
+            string output, errors;
+
+            Process proc = null;
+
+            // @todo ADB socket
+
+            try
+            {
+                proc = Process.Start(startInfo);
+
+                errors = proc.StandardError.ReadToEnd();
+                if (!String.IsNullOrEmpty(errors)) errors = errors.Trim();
+                
+                output = proc.StandardOutput.ReadToEnd();
+                if (!String.IsNullOrEmpty(output)) output = output.Trim();
+            }
+            catch (Exception ex)
+            {
+                Debug.Print(ex.Message);
+                return;
+            }
+            finally
+            {
+                if (proc != null) proc.Dispose();
+            }
+
+            if (Int32.TryParse(output, out level))
+            {
+                formProgressBar.Style = ProgressBarStyle.Blocks;
+                formProgressBar.Value = level;
+
+                Text = String.Format("[ {0}% ]", level);
+
+                UpdateTaskbar(true);
+            }
+            else
+            {
+                formProgressBar.Style = ProgressBarStyle.Marquee;
+                Text = errors;
+
+                UpdateTaskbar(false);
+            }
+
+        }
+        private void UpdateTaskbar(bool HasValue) {
+
+            if (!TaskbarManager.IsPlatformSupported) return;
+            if (!TaskBarAvailable) return;
+
+            if (HasValue)
+                shellProgressBar.SetProgressValue(level, 100);
+
+            if (!HasValue)
+                shellProgressBar.SetProgressState(TaskbarProgressBarState.Indeterminate);
+            else if (level < 15)
+                shellProgressBar.SetProgressState(TaskbarProgressBarState.Error);
+            else if (level < 90)
+                shellProgressBar.SetProgressState(TaskbarProgressBarState.Paused);
+            else
+                shellProgressBar.SetProgressState(TaskbarProgressBarState.Normal);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            WindowState = FormWindowState.Minimized;
+            Visible = false;
+
+            formProgressBar.Style = ProgressBarStyle.Marquee;
+            formProgressBar.Maximum = 100;
+        }
+
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+            shellProgressBar.SetProgressState(TaskbarProgressBarState.Indeterminate);
+            shellProgressBar.SetApplicationIdForSpecificWindow(Handle, "MainWnd");
+
+            TaskBarAvailable = true;
+
+            timer_Tick(null, null);
+
+            timer.Interval = TIMER_INTERVAL;
+            timer.Tick += timer_Tick;
+            timer.Enabled = true;
+        }
+    }
+
+    internal class ValueProgressBar : ProgressBar
+    {
+        /*
+        public ValueProgressBar()
+        {
+            this.SetStyle(ControlStyles.UserPaint, true);
+        }
+        
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            Rectangle rec = e.ClipRectangle;
+            rec.Width = (int)(rec.Width * ((double)Value / Maximum)) - 4;
+
+            if (ProgressBarRenderer.IsSupported)
+                ProgressBarRenderer.DrawHorizontalBar(e.Graphics, e.ClipRectangle);
+            rec.Height = rec.Height - 4;
+            e.Graphics.FillRectangle(Brushes.Red, 2, 2, rec.Width, rec.Height);
+        }
+         * */
+    }
+}
